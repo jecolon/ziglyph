c100011e7a76dead84be1eeee82699f4a728611d
Jose E. Colon Rodriguez <jec.rod@gmail.com>
2021-04-14 15:32:49 -0400

    Updated README

--- README.md
+++ README.md
@@ -108,32 +108,53 @@ test "Component structs" {
 ## Decomposition and Normalization
 In addition to the basic functions to detect and convert code point case, the `DecomposeMap` struct 
 provides code point decomposition and string normalization methods. This library currently only 
-performs full compatibility decomposition and normalization (NFKD). Future versions may add more
-normalization forms.
+performs full canonical and compatibility decomposition and normalization (NFD and NFKD). Future 
+versions may add more normalization forms.
 
 ```zig
 const std = @import("std");
+const expect = std.testing.expect;
+const expectEqual = std.testing.expectEqual;
 const expectEqualSlices = std.testing.expectEqualSlices;
 
 // Import struct.
 const DecomposeMap = @import("ziglyph.zig").DecomposeMap;
+const Decomposed = DecomposedMap.Decomposed;
 
-test "decomposeCodePoint" {
+test "decomposeCD" {
     var allocator = std.testing.allocator;
     var z = try DecomposeMap.init(allocator);
     defer z.deinit();
 
-    // Given the recursive nature of the decomposition / normalization process,
-    // an arena allocator is strongly recommended for easy memory management.
     var arena = std.heap.ArenaAllocator.init(allocator);
     defer arena.deinit();
     var arena_allocator = &arena.allocator;
 
-    var result = try z.decompose(arena_allocator, '\u{00E9}');
-    expectEqualSlices(u21, result, &[2]u21{ 0x0065, 0x0301 });
+    // CD: ox03D3 -> 0x03D2, 0x0301
+    var src = [1]Decomposed{.{ .src = '\u{03D3}' }};
+    var result = try z.decomposeCD(arena_allocator, &src);
+    expectEqual(result.len, 2);
+    expectEqual(result[0].same, 0x03D2);
+    expectEqual(result[1].same, 0x0301);
+}
 
-    result = try z.decompose(arena_allocator, '\u{03D3}');
-    expectEqualSlices(u21, result, &[2]u21{ 0x03A5, 0x0301 });
+test "decomposeKD" {
+    var allocator = std.testing.allocator;
+    var z = try DecomposeMap.init(allocator);
+    defer z.deinit();
+
+    var arena = std.heap.ArenaAllocator.init(allocator);
+    defer arena.deinit();
+    var arena_allocator = &arena.allocator;
+
+    // KD: ox03D3 -> 0x03D2, 0x0301 -> 0x03A5, 0x0301
+    var src = [1]Decomposed{.{ .src = '\u{03D3}' }};
+    var result = try z.decomposeKD(arena_allocator, &src);
+    expectEqual(result.len, 2);
+    expect(result[0] == .same);
+    expectEqual(result[0].same, 0x03A5);
+    expect(result[1] == .same);
+    expectEqual(result[1].same, 0x0301);
 }
 
 test "normalize" {
@@ -145,12 +166,16 @@ test "normalize" {
     defer arena.deinit();
     var arena_allocator = &arena.allocator;
 
-    // Note that this performs a full, recursive, compatibility normalization.
-    // Source -> Canonical (NFD) -> Compatibility (NFKD)
-    // 0x03D3 -> 0x03D2, 0x0301  -> 0x03A5, 0x0301
-    const input = "H\u{03D3}llo";
-    const want = "H\u{03A5}\u{0301}llo";
-    const got = try z.normalize(arena_allocator, input);
+    // Canonical (NFD)
+    var input = "Complex char: \u{03D3}";
+    var want = "Complex char: \u{03D2}\u{0301}";
+    var got = try z.toNFD(arena_allocator, input);
+    expectEqualSlices(u8, want, got);
+
+    // Compatibility (NFKD)
+    input = "Complex char: \u{03D3}";
+    want = "Complex char: \u{03A5}\u{0301}";
+    got = try z.toNFKD(arena_allocator, input);
     expectEqualSlices(u8, want, got);
 }
 ```
--- src/readme_tests.zig
+++ src/readme_tests.zig
@@ -3,91 +3,64 @@ const expect = std.testing.expect;
 const expectEqual = std.testing.expectEqual;
 const expectEqualSlices = std.testing.expectEqualSlices;
 
-// Import the struct.
-const Ziglyph = @import("ziglyph.zig").Ziglyph;
-
-test "Ziglyph struct" {
-    var ziglyph = try Ziglyph.init(std.testing.allocator);
-    defer ziglyph.deinit();
-
-    const z = 'z';
-    // Lazy init requires 'try'
-    expect(try ziglyph.isLetter(z));
-    expect(try ziglyph.isAlphaNum(z));
-    expect(try ziglyph.isPrint(z));
-    expect(!try ziglyph.isUpper(z));
-    const uz = try ziglyph.toUpper(z);
-    expect(try ziglyph.isUpper(uz));
-    expectEqual(uz, 'Z');
-}
-
-// Import the struct.
-const Letter = @import("ziglyph.zig").Letter;
-const Punct = @import("ziglyph.zig").Punct;
-
-test "Ziglyph struct" {
-    var letter = try Letter.init(std.testing.allocator);
-    defer letter.deinit();
-    var punct = try Punct.init(std.testing.allocator);
-    defer punct.deinit();
-
-    const z = 'z';
-    // Aggregate structs also use lezy init.
-    expect(try letter.isLetter(z));
-    expect(!try letter.isUpper(z));
-    expect(!try punct.isPunct(z));
-    expect(try punct.isPunct('!'));
-    const uz = try letter.toUpper(z);
-    expect(try letter.isUpper(uz));
-    expectEqual(uz, 'Z');
-}
-
-// Import the components.
-const Lower = @import("ziglyph.zig").Letter.Lower;
-const Upper = @import("ziglyph.zig").Letter.Upper;
-// Case mapping.
-const UpperMap = @import("ziglyph.zig").Letter.UpperMap;
-
-test "Component structs" {
-    var lower = try Lower.init(std.testing.allocator);
-    defer lower.deinit();
-    var upper = try Upper.init(std.testing.allocator);
-    defer upper.deinit();
-    var upper_map = try UpperMap.init(std.testing.allocator);
-    defer upper_map.deinit();
-
-    const z = 'z';
-    // No lazy init, no 'try' here.
-    expect(lower.isLowercaseLetter(z));
-    expect(!upper.isUppercaseLetter(z));
-    const uz = upper_map.toUpper(z);
-    expect(upper.isUppercaseLetter(uz));
-    expectEqual(uz, 'Z');
-}
-
 // Import struct.
 const DecomposeMap = @import("ziglyph.zig").DecomposeMap;
+const Decomposed = DecomposeMap.Decomposed;
 
-test "decomposeCodePoint" {
-    var decomp_map = try DecomposeMap.init(std.testing.allocator);
-    defer decomp_map.deinit();
+test "decomposeCD" {
+    var allocator = std.testing.allocator;
+    var z = try DecomposeMap.init(allocator);
+    defer z.deinit();
 
-    var result = decomp_map.decomposeCodePoint('\u{00E9}');
-    switch (result) {
-        .same => @panic("Expected .seq, got .same for \\u{00E9}"),
-        .seq => |seq| expectEqualSlices(u21, seq, &[_]u21{ '\u{0065}', '\u{0301}' }),
-    }
+    var arena = std.heap.ArenaAllocator.init(allocator);
+    defer arena.deinit();
+    var arena_allocator = &arena.allocator;
+
+    // CD: ox03D3 -> 0x03D2, 0x0301
+    var src = [1]Decomposed{.{ .src = '\u{03D3}' }};
+    var result = try z.decomposeCD(arena_allocator, &src);
+    expectEqual(result.len, 2);
+    expectEqual(result[0].same, 0x03D2);
+    expectEqual(result[1].same, 0x0301);
 }
 
-test "decomposeString" {
-    var decomp_map = try DecomposeMap.init(std.testing.allocator);
-    defer decomp_map.deinit();
+test "decomposeKD" {
+    var allocator = std.testing.allocator;
+    var z = try DecomposeMap.init(allocator);
+    defer z.deinit();
 
-    const input = "H\u{00E9}llo";
-    const want = "H\u{0065}\u{0301}llo";
-    // decomposeString allocates memory for the returned slice, so it can fail.
-    const got = try decomp_map.decomposeString(input);
-    // We must free the returned slice when done.
-    defer std.testing.allocator.free(got);
+    var arena = std.heap.ArenaAllocator.init(allocator);
+    defer arena.deinit();
+    var arena_allocator = &arena.allocator;
+
+    // KD: ox03D3 -> 0x03D2, 0x0301 -> 0x03A5, 0x0301
+    var src = [1]Decomposed{.{ .src = '\u{03D3}' }};
+    var result = try z.decomposeKD(arena_allocator, &src);
+    expectEqual(result.len, 2);
+    expect(result[0] == .same);
+    expectEqual(result[0].same, 0x03A5);
+    expect(result[1] == .same);
+    expectEqual(result[1].same, 0x0301);
+}
+
+test "normalize" {
+    var allocator = std.testing.allocator;
+    var z = try DecomposeMap.init(allocator);
+    defer z.deinit();
+
+    var arena = std.heap.ArenaAllocator.init(allocator);
+    defer arena.deinit();
+    var arena_allocator = &arena.allocator;
+
+    // Canonical (NFD)
+    var input = "Complex char: \u{03D3}";
+    var want = "Complex char: \u{03D2}\u{0301}";
+    var got = try z.toNFD(arena_allocator, input);
+    expectEqualSlices(u8, want, got);
+
+    // Compatibility (NFKD)
+    input = "Complex char: \u{03D3}";
+    want = "Complex char: \u{03A5}\u{0301}";
+    got = try z.toNFKD(arena_allocator, input);
     expectEqualSlices(u8, want, got);
 }
