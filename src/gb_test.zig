d99f95be6dc9338a500dd445aa8249a017f6bc16
Jose E. Colon Rodriguez <jec.rod@gmail.com>
2021-04-18 12:39:41 -0400

    Much better grapheme break impl

--- src/gb_test.zig
+++ src/gb_test.zig
@@ -92,276 +92,176 @@ test "grapheme break" {
 
         // Special code points.
         const ZWJ: u21 = 0x200D;
-        const CGJ: u21 = 0x034F;
         const CR: u21 = 0x000D;
         const LF: u21 = 0x000A;
 
         var char_start: usize = 0;
+        var emoji_seen = false;
         var i: usize = 0;
-        while (i < len) : (i += 1) {
+        while (i < len) {
+            const i_at_top = i;
+            std.debug.print("line {d} i_at_top: {d}\n", .{ line_no, i_at_top });
             const cp = items[i];
             const next: ?u21 = if (i < len - 1) items[i + 1] else null;
             const prev: ?u21 = if (i > 0) items[i - 1] else null;
 
             // Apply rules.
-            if (extpic.isExtendedPictographic(cp)) {
-                // GB11
-                if (prev) |pcp| {
-                    // GB9b
-                    if (!prepend.isPrepend(pcp) and char_start < i) {
-                        // Add previous.
-                        try got.append(try Zigchar.fromCodePoints(allocator, items[char_start..i]));
-                        char_start = i;
-                    }
-                }
-
+            // GB3
+            if (cp == CR) {
                 if (next) |ncp| {
-                    var has_extend = false;
-                    if (ncp == ZWJ) {
-                        var j = i + 2;
-                        if (j < len and extpic.isExtendedPictographic(items[j])) {
-                            // Extpic + ZWJ + Extpic
-                            i += 2;
-                            var k = i + 1;
-                            if (k < len and (extend.isExtend(items[k]) or spacing.isSpacingMark(items[k]))) {
-                                // Possible run?
-                                var l = k + 1;
-                                while (l < len and (extend.isExtend(items[l]) or spacing.isSpacingMark(items[l]))) : (l += 1) {}
-                                if (l > k + 1) i = l - 1 else i = k; // backup
-                            }
-                        } else {
-                            // Extpic + ZWJ
-                            i += 1;
-                        }
-                    } else if (extend.isExtend(ncp) or spacing.isSpacingMark(ncp)) {
-                        // Possible run?
-                        i += 1;
-                        var j = i + 1; // known
-                        while (j < len and (extend.isExtend(items[j]) or spacing.isSpacingMark(items[j]))) : (j += 1) {}
-                        if (j > i + 1) i = j - 1; // backup
-                        has_extend = true;
-                    }
-
-                    if (has_extend) {
-                        var j = i + 1;
-                        if (j < len and items[j] == ZWJ) {
-                            // Maybe Extpic + Extend + ZWJ + Extpic?
-                            j += 1;
-                            if (j < len and extpic.isExtendedPictographic(items[j])) {
-                                // Yes!
-                                i += 2;
-                                var k = i + 1;
-                                if (k < len and (extend.isExtend(items[k]) or spacing.isSpacingMark(items[k]))) {
-                                    // Possible run?
-                                    var l = k + 1;
-                                    while (l < len and (extend.isExtend(items[l]) or spacing.isSpacingMark(items[l]))) : (l += 1) {}
-                                    if (l > k + 1) i = l - 1 else i = k; // backup
-                                }
-                            } else {
-                                // No, Extpic + Extend + ZWJ only.
-                                i += 1;
-                            }
-                        }
+                    if (ncp == LF) {
+                        i += 2;
+                        std.debug.print("line {d} CRLF: {d} {d}\n", .{ line_no, i, char_start });
+                        continue;
                     }
                 }
-
-                try got.append(try Zigchar.fromCodePoints(allocator, items[char_start .. i + 1]));
-                char_start = i + 1;
-                continue;
             }
 
-            if (regional.isRegionalIndicator(cp)) {
-                // GB12, GB13
-                if (prev) |pcp| {
-                    // GB9b
-                    if (!prepend.isPrepend(pcp) and char_start < i) {
-                        // Add previous.
-                        try got.append(try Zigchar.fromCodePoints(allocator, items[char_start..i]));
-                        char_start = i;
-                    }
-                }
-
-                if (next) |ncp| {
-                    // GB3
-                    if (regional.isRegionalIndicator(ncp)) {
-                        i += 1;
-                        var j = i + 1; // i is known
-                        if (j < len and (extend.isExtend(items[j]) or items[j] == ZWJ or spacing.isSpacingMark(items[j]))) {
-                            // Possible run of extenders.
-                            j += 1;
-                            while (j < len and (extend.isExtend(items[j]) or items[j] == ZWJ or spacing.isSpacingMark(items[j]))) : (j += 1) {}
-                            if (j > i + 1) i = j - 1; // backup
-                        }
-                    } else if (extend.isExtend(ncp) or ncp == ZWJ or spacing.isSpacingMark(ncp)) {
-                        // Possible run of extenders.
-                        i += 1;
-                        var j = i + 1; // known
-                        while (j < len and (extend.isExtend(items[j]) or items[j] == ZWJ or spacing.isSpacingMark(items[j]))) : (j += 1) {}
-                        if (j > i + 1) i = j - 1; // backup
-                    }
-                }
-
-                try got.append(try Zigchar.fromCodePoints(allocator, items[char_start .. i + 1]));
-                char_start = i + 1;
-                continue;
-            }
-
-            if (cp == CR or cp == LF) {
-                // GB3, GB4, GB5
-                if (char_start < i) {
-                    // Add previous.
-                    try got.append(try Zigchar.fromCodePoints(allocator, items[char_start..i]));
-                }
-                char_start = i;
-                if (next) |ncp| {
-                    // GB3
-                    if (cp == CR and ncp == LF) i += 1;
-                }
-                try got.append(try Zigchar.fromCodePoints(allocator, items[char_start .. i + 1]));
-                char_start = i + 1;
-                continue;
-            }
-
-            if (control.isControl(cp)) {
-                // GB4, GB5
-                if (char_start < i) {
-                    // Add previous.
-                    try got.append(try Zigchar.fromCodePoints(allocator, items[char_start..i]));
-                }
-                char_start = i;
-                try got.append(try Zigchar.fromCodePoints(allocator, items[char_start .. i + 1]));
-                char_start = i + 1;
-                continue;
-            }
-
+            // GB6, GB7, GB8
             if (han_map.syllableType(cp)) |hst| {
-                // GB6, GB7, GB8
-                if (prev) |pcp| {
-                    // GB9b
-                    if (!prepend.isPrepend(pcp) and char_start < i) {
-                        // Add previous.
-                        try got.append(try Zigchar.fromCodePoints(allocator, items[char_start..i]));
-                        char_start = i;
-                    }
-                }
-
                 if (next) |ncp| {
                     const ncp_hst = han_map.syllableType(ncp);
+
                     if (ncp_hst) |nhst| {
                         switch (hst) {
                             .L => {
-                                if (nhst == .L or nhst == .V or nhst == .LV or nhst == .LVT) i += 1;
+                                if (nhst == .L or nhst == .V or nhst == .LV or nhst == .LVT) {
+                                    i += 2;
+                                    std.debug.print("line {d} .L: {d} {d}\n", .{ line_no, i, char_start });
+                                    continue;
+                                }
                             },
                             .LV, .V => {
-                                if (nhst == .V or nhst == .T) i += 1;
+                                if (nhst == .V or nhst == .T) {
+                                    i += 2;
+                                    std.debug.print("line {d} .LV, .V: {d} {d}\n", .{ line_no, i, char_start });
+                                    continue;
+                                }
                             },
                             .LVT, .T => {
-                                if (nhst == .T) i += 1;
+                                if (nhst == .T) {
+                                    i += 2;
+                                    std.debug.print("line {d} .LVT, .T: {d} {d}\n", .{ line_no, i, char_start });
+                                    continue;
+                                }
                             },
                         }
                     }
                 }
             }
 
-            if (next) |ncp| {
-                // GB9, GB9a
-                if (extend.isExtend(ncp) or ncp == ZWJ or spacing.isSpacingMark(ncp)) {
-                    i += 1;
-                    continue;
-                }
-            }
-
-            if (prev != null) {
-                // GB9, GB9a
-                if (extend.isExtend(cp) or cp == ZWJ or spacing.isSpacingMark(cp)) {
-                    continue;
-                }
-            }
-
-            if (prepend.isPrepend(cp)) {
-                // GB9b
-                if (prev) |pcp| {
-                    if (!prepend.isPrepend(pcp)) {
-                        if (char_start < i) {
-                            // Add previous.
-                            try got.append(try Zigchar.fromCodePoints(allocator, items[char_start..i]));
-                            char_start = i;
-                        }
-                    } else {
-                        // Possible run?
-                        i += 1; // known
-                        while (i < len and prepend.isPrepend(items[i])) : (i += 1) {}
-                        i -= 1; // backup
-                    }
-                }
+            // GB9
+            if (extend.isExtend(cp)) {
+                i += lexRun(items[i..], extend, Extend.isExtend);
+                std.debug.print("line {d} Post-Extend: {d} {d}\n", .{ line_no, i, char_start });
                 continue;
             }
-
-            //if (regional.isRegionalIndicator(cp)) {
-            //    // GB12, GB13
-            //    if (prev) |pcp| {
-            //        // GB9b
-            //        if (!prepend.isPrepend(pcp) and !regional.isRegionalIndicator(pcp) and char_start < i) {
-            //            // Add previous.
-            //            try got.append(try Zigchar.fromCodePoints(allocator, items[char_start..i]));
-            //            char_start = i;
-            //        }
-            //    }
-
-            //    if (next) |ncp| {
-            //        // GB9, GB9a
-            //        if (regional.isRegionalIndicator(ncp)) {
-            //            if (prev) |pcp| {
-            //                if (regional.isRegionalIndicator(pcp)) {
-            //                    // GB12, GB13
-            //                    var ri_count: usize = 0;
-            //                    var j = i - 1; // known
-            //                    while (regional.isRegionalIndicator(items[j])) : (j -= 1) {
-            //                        ri_count += 1;
-            //                        if (j == 0) break;
-            //                    }
-            //                    // GB13
-            //                    if (ri_count % 2 != 0) continue;
-            //                } else {
-            //                    // GB13 non-RI at sot.
-            //                    continue;
-            //                }
-            //            } else {
-            //                // GB12 at sot.
-            //                continue;
-            //            }
-            //        }
-            //    }
-
-            //    //try got.append(try Zigchar.fromCodePoints(allocator, items[char_start .. i + 1]));
-            //    //char_start = i + 1;
-            //    //continue;
-            //}
-
-            // GB 999
-            if (prev) |pcp| {
-                // GB9b
-                if (!prepend.isPrepend(pcp) and !regional.isRegionalIndicator(pcp) and char_start < i) {
-                    // Add previous.
-                    try got.append(try Zigchar.fromCodePoints(allocator, items[char_start..i]));
-                    char_start = i;
+            if (spacing.isSpacingMark(cp)) {
+                i += lexRun(items[i..], spacing, SpacingMark.isSpacingMark);
+                std.debug.print("line {d} Post-Spacing: {d} {d}\n", .{ line_no, i, char_start });
+                continue;
+            }
+            if (cp == ZWJ) {
+                if (prev != null) {
+                    i += 1;
+                    std.debug.print("line {d} Post-ZWJ: {d} {d}\n", .{ line_no, i, char_start });
+                    continue;
                 }
+            }
+
+            // GB9.2
+            if (prepend.isPrepend(cp)) {
+                if (next) |ncp| {
+                    if (ncp != CR and ncp != LF and !control.isControl(ncp)) {
+                        i += 2;
+                        std.debug.print("line {d} Prepend: {d} {d}\n", .{ line_no, i, char_start });
+                        continue;
+                    }
+                }
+            }
+
+            // GB11
+            if (extpic.isExtendedPictographic(cp)) {
+                if (emoji_seen) {
+                    if (prev) |pcp| {
+                        if (pcp == ZWJ) {
+                            i += 1;
+                            std.debug.print("line {d} Emoji-ZWJ: {d} {d}\n", .{ line_no, i, char_start });
+                            emoji_seen = false;
+                            continue;
+                        }
+                    }
+                } else {
+                    // Emoji marks new cluster.
+                    if (prev != null and char_start < i_at_top) {
+                        try got.append(try Zigchar.fromCodePoints(allocator, items[char_start..i_at_top]));
+                        char_start = i_at_top;
+                    }
+                    emoji_seen = true;
+                    std.debug.print("line {d} Emoji-not-seen: {d} {d}\n", .{ line_no, i, char_start });
+                    continue;
+                }
+            }
+
+            // GB12
+            if (regional.isRegionalIndicator(cp)) {
+                if (next) |ncp| {
+                    if (regional.isRegionalIndicator(ncp)) {
+                        // Only 2 code points in RI sequences.
+                        if (prev != null and char_start < i_at_top) {
+                            try got.append(try Zigchar.fromCodePoints(allocator, items[char_start..i_at_top]));
+                            char_start = i_at_top;
+                        }
+                        i += 2;
+                        std.debug.print("line {d} RI: {d} {d}\n", .{ line_no, i, char_start });
+                        continue;
+                    }
+                }
+            }
+
+            // GB9 continued.
+            if (cp != CR and cp != LF and !control.isControl(cp)) {
+                if (next) |ncp| {
+                    if (extend.isExtend(ncp) or spacing.isSpacingMark(ncp) or ncp == ZWJ) {
+                        i += 1;
+                        std.debug.print("line {d} Skip append: {d} {d}\n", .{ line_no, i, char_start });
+                        continue;
+                    }
+                }
+            }
+
+            // GB999
+            // Add any pending char.
+            if (prev != null and char_start < i) {
+                try got.append(try Zigchar.fromCodePoints(allocator, items[char_start..i]));
+                char_start = i;
             }
 
             try got.append(try Zigchar.fromCodePoints(allocator, items[char_start .. i + 1]));
-            char_start = i + 1;
+            i += 1;
+            std.debug.print("line {d} Bottom: {d} {d}\n", .{ line_no, i, char_start });
+            char_start = i;
         }
 
-        // Add last one.
+        // Any left over?
         if (char_start < i) {
-            try got.append(try Zigchar.fromCodePoints(allocator, items[char_start..i]));
+            try got.append(try Zigchar.fromCodePoints(allocator, items[char_start..]));
         }
         /////////////////////////////////////////
 
         // Chaeck.
         for (want.items) |char, j| {
-            std.debug.print("line {d}: ({s}) -> ({s})\n", .{ line_no, char.bytes, got.items[j].bytes });
             expectEqualSlices(u8, char.bytes, got.items[j].bytes);
         }
     }
+}
+
+fn lexRun(
+    list: []const u21,
+    ctx: anytype,
+    comptime predicate: fn (ctx: @TypeOf(ctx), cp: u21) bool,
+) usize {
+    return for (list) |cp, i| {
+        if (!predicate(ctx, cp)) break i;
+    } else i;
 }
