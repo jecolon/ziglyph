1202293092d440ea8d4d2533f11cbb7a52e0078b
Jose Colon Rodriguez <jec.rod@gmail.com>
2022-11-19 10:29:22 -0400

    Simplified GraphemeIterator impl

--- src/segmenter/Grapheme.zig
+++ src/segmenter/Grapheme.zig
@@ -22,193 +22,120 @@ pub fn eql(self: Grapheme, str: []const u8) bool {
     return mem.eql(u8, self.bytes, str);
 }
 
-const Type = enum {
-    control,
-    cr,
-    extend,
-    han_l,
-    han_lv,
-    han_lvt,
-    han_t,
-    han_v,
-    lf,
-    prepend,
-    regional,
-    spacing,
-    xpic,
-    zwj,
-    any,
-
-    fn get(cp: CodePoint) Type {
-        var ty: Type = .any;
-        if (0x000D == cp.scalar) ty = .cr;
-        if (0x000A == cp.scalar) ty = .lf;
-        if (0x200D == cp.scalar) ty = .zwj;
-        if (gbp.isControl(cp.scalar)) ty = .control;
-        if (gbp.isExtend(cp.scalar)) ty = .extend;
-        if (gbp.isL(cp.scalar)) ty = .han_l;
-        if (gbp.isLv(cp.scalar)) ty = .han_lv;
-        if (gbp.isLvt(cp.scalar)) ty = .han_lvt;
-        if (gbp.isT(cp.scalar)) ty = .han_t;
-        if (gbp.isV(cp.scalar)) ty = .han_v;
-        if (gbp.isPrepend(cp.scalar)) ty = .prepend;
-        if (gbp.isRegionalIndicator(cp.scalar)) ty = .regional;
-        if (gbp.isSpacingmark(cp.scalar)) ty = .spacing;
-        if (emoji.isExtendedPictographic(cp.scalar)) ty = .xpic;
-
-        return ty;
-    }
-};
-
-const Token = struct {
-    ty: Type,
-    code_point: CodePoint,
-
-    fn is(self: Token, ty: Type) bool {
-        return self.ty == ty;
-    }
-};
-
 /// `GraphemeIterator` iterates a sting one grapheme cluster at-a-time.
 pub const GraphemeIterator = struct {
+    buf: [2]?CodePoint = [_]?CodePoint{ null, null },
     cp_iter: CodePointIterator,
-    current: ?Token = null,
-    start: ?Token = null,
 
     const Self = @This();
 
     pub fn init(str: []const u8) !Self {
         if (!unicode.utf8ValidateSlice(str)) return error.InvalidUtf8;
-        return Self{ .cp_iter = CodePointIterator{ .bytes = str } };
+        var self = Self{ .cp_iter = CodePointIterator{ .bytes = str } };
+        self.buf[1] = self.cp_iter.next();
+
+        return self;
     }
 
-    // Main API.
     pub fn next(self: *Self) ?Grapheme {
-        if (self.advance()) |latest_non_ignorable| {
-            var end = self.current.?;
+        const cp = self.advance() orelse return null;
+        const start = cp.offset;
+        var end = cp.end();
 
-            if (isBreaker(latest_non_ignorable)) {
-                if (latest_non_ignorable.is(.cr)) {
-                    if (self.peek()) |p| {
-                        // GB3
-                        if (p.is(.lf)) {
-                            _ = self.advance();
-                            end = self.current.?;
-                        }
-                    }
+        if (cp.scalar == '\x0d') {
+            if (self.peek()) |pcp| {
+                if (pcp.scalar == '\x0a') {
+                    end = pcp.end();
+                    _ = self.advance();
                 }
             }
 
-            if (latest_non_ignorable.is(.regional)) {
-                if (self.peek()) |p| {
-                    // GB12
-                    if (p.is(.regional) and !isIgnorable(end)) {
-                        _ = self.advance();
-                        end = self.current.?;
-                    }
-                }
-            }
-
-            if (latest_non_ignorable.is(.han_l)) {
-                if (self.peek()) |p| {
-                    // GB6
-                    if ((p.is(.han_l) or p.is(.han_v) or p.is(.han_lv) or p.is(.han_lvt)) and
-                        !isIgnorable(end))
-                    {
-                        _ = self.advance();
-                        end = self.current.?;
-                    }
-                }
-            }
-
-            if (latest_non_ignorable.is(.han_lv) or latest_non_ignorable.is(.han_v)) {
-                if (self.peek()) |p| {
-                    // GBy
-                    if ((p.is(.han_v) or p.is(.han_t)) and !isIgnorable(end)) {
-                        _ = self.advance();
-                        end = self.current.?;
-                    }
-                }
-            }
-
-            if (latest_non_ignorable.is(.han_lvt) or latest_non_ignorable.is(.han_t)) {
-                if (self.peek()) |p| {
-                    // GB8
-                    if (p.is(.han_t) and !isIgnorable(end)) {
-                        _ = self.advance();
-                        end = self.current.?;
-                    }
-                }
-            }
-
-            if (latest_non_ignorable.is(.xpic)) {
-                if (self.peek()) |p| {
-                    // GB11
-                    if (p.is(.xpic) and end.is(.zwj)) {
-                        _ = self.advance();
-                        end = self.current.?;
-                    }
-                }
-            }
-
-            const start = self.start.?;
-            self.start = self.peek();
-
-            // GB999
-            return self.emit(start, end);
+            return Grapheme{
+                .bytes = self.cp_iter.bytes[start..end],
+                .offset = start,
+            };
         }
 
-        return null;
-    }
+        if (cp.scalar == '\x0a' or gbp.isControl(cp.scalar)) {
+            return Grapheme{
+                .bytes = self.cp_iter.bytes[start..end],
+                .offset = start,
+            };
+        }
 
-    fn peek(self: *Self) ?Token {
-        const saved_i = self.cp_iter.i;
-        defer self.cp_iter.i = saved_i;
-
-        return if (self.cp_iter.next()) |cp| Token{
-            .ty = Type.get(cp),
-            .code_point = cp,
-        } else null;
-    }
-
-    fn advance(self: *Self) ?Token {
-        const latest_non_ignorable = if (self.cp_iter.next()) |cp| Token{
-            .ty = Type.get(cp),
-            .code_point = cp,
-        } else return null;
-
-        self.current = latest_non_ignorable;
-        if (self.start == null) self.start = latest_non_ignorable; // Happens only at beginning.
-
-        // GB9b
-        if (latest_non_ignorable.is(.prepend)) {
-            if (self.peek()) |p| {
-                if (!isBreaker(p)) return self.advance();
+        if (gbp.isPrepend(cp.scalar)) {
+            if (self.peek()) |pcp| {
+                if (!cpIsBreaker(pcp.scalar)) {
+                    end = pcp.end();
+                    _ = self.advance();
+                }
             }
         }
-        // GB9, GBia
-        // NOTE: This may increment self.i, making self.current a different token then latest_non_ignorable.
-        if (!isBreaker(latest_non_ignorable)) self.skipIgnorables();
 
-        return latest_non_ignorable;
-    }
-
-    fn skipIgnorables(self: *Self) void {
-        while (self.peek()) |peek_token| {
-            if (!isIgnorable(peek_token)) break;
-            _ = self.advance();
+        if (gbp.isRegionalIndicator(cp.scalar)) {
+            if (self.peek()) |pcp| {
+                if (gbp.isRegionalIndicator(pcp.scalar)) {
+                    end = pcp.end();
+                    _ = self.advance();
+                }
+            }
         }
-    }
 
-    // Production.
-    fn emit(self: Self, start_token: Token, end_token: Token) Grapheme {
-        const start = start_token.code_point.offset;
-        const end = end_token.code_point.end();
+        if (gbp.isL(cp.scalar)) {
+            if (self.peek()) |pcp| {
+                if (gbp.isL(pcp.scalar) or gbp.isV(pcp.scalar) or gbp.isLv(pcp.scalar) or gbp.isLvt(pcp.scalar)) {
+                    end = pcp.end();
+                    _ = self.advance();
+                }
+            }
+        }
 
-        return .{
+        if (gbp.isLv(cp.scalar) or gbp.isV(cp.scalar)) {
+            if (self.peek()) |pcp| {
+                if (gbp.isV(pcp.scalar) or gbp.isT(pcp.scalar)) {
+                    end = pcp.end();
+                    _ = self.advance();
+                }
+            }
+        }
+
+        if (gbp.isLvt(cp.scalar) or gbp.isT(cp.scalar)) {
+            if (self.peek()) |pcp| {
+                if (gbp.isT(pcp.scalar)) {
+                    end = pcp.end();
+                    _ = self.advance();
+                }
+            }
+        }
+
+        const after_emoji = emoji.isExtendedPictographic(cp.scalar);
+        var after_zwj = false;
+
+        while (self.peek()) |pcp| {
+            if (cpIsIgnorable(pcp.scalar) or (after_emoji and after_zwj and emoji.isExtendedPictographic(pcp.scalar))) {
+                end = pcp.end();
+                _ = self.advance();
+                if (pcp.scalar == '\u{200d}') after_zwj = true;
+            } else {
+                break;
+            }
+        }
+
+        return Grapheme{
             .bytes = self.cp_iter.bytes[start..end],
             .offset = start,
         };
+    }
+
+    fn advance(self: *Self) ?CodePoint {
+        self.buf[0] = self.buf[1];
+        self.buf[1] = self.cp_iter.next();
+
+        return self.buf[0];
+    }
+
+    fn peek(self: Self) ?CodePoint {
+        return self.buf[1];
     }
 };
 
@@ -340,20 +267,6 @@ pub fn StreamingGraphemeIterator(comptime T: type) type {
 }
 
 // Predicates
-const TokenPredicate = fn (Token) bool;
-
-fn isBreaker(token: Token) bool {
-    return token.ty == .control or token.ty == .cr or token.ty == .lf;
-}
-
-fn isControl(token: Token) bool {
-    return token.ty == .control;
-}
-
-fn isIgnorable(token: Token) bool {
-    return token.ty == .extend or token.ty == .spacing or token.ty == .zwj;
-}
-
 const CodePointPredicate = fn (u21) bool;
 
 fn cpIsBreaker(cp: u21) bool {
